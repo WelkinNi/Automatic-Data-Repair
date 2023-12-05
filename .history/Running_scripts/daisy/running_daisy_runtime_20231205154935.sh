58691309946363e6a9aff9a1c1ace52ab65cdc79
WelkinNi <834381638@qq.com>
2023-12-05 15:49:37 +0800

    update

--- .history/Running_scripts/daisy/running_daisy_runtime_20231205154935.sh
+++ .history/Running_scripts/daisy/running_daisy_runtime_20231205154935.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 TASK_LIST=("tax")
 CNTS_LIST=(1 2 3)
+NUMS_LIST=()
 for ((i=10; i<=50; i+=10)); 
 do
   NUMS_LIST+=("$(printf "%04d" $i)k") 
@@ -8,7 +9,7 @@ done
 
 for CNT in "${CNTS_LIST[@]}"
 do
-    PYTHON="./Unified/Unified.py"
+    PYTHON="./Daisy/daisy.py"
     # Loop through error rates and run holoclean_run.py on dataset
     for NUM in "${NUMS_LIST[@]}"
     do
@@ -17,12 +18,11 @@ do
         do 
             DIRTY_DATA="./data_with_rules/tax/split_data/tax-dirty-original_error-${NUM}.csv"
             CLEAN_DATA="./data_with_rules/tax/split_data/tax-clean-clean_data_ori-${NUM}.csv"
-            RULE="./data_with_rules/${TASK}/dc_rules-validate-fd-horizon.txt"
+            RULE="./data_with_rules/${TASK}/dc_rules_holoclean.txt"
                     
             TASK_NAME="${TASK}${CNT}"
             DIRTY_DATA_PATH=${DIRTY_DATA}
-            
-            /home/dell/anaconda3/envs/torch110/bin/python $PYTHON --task_name "$TASK_NAME" --rule_path "$RULE" --onlyed 0 --perfected 0 --dirty_path "$DIRTY_DATA_PATH" --clean_path "$CLEAN_DATA" &
+            /home/dell/anaconda3/envs/torch110/bin/python $PYTHON --task_name "$TASK_NAME" --rule_path "$RULE" --onlyed 0 --perfected 0 --dirty_path "$DIRTY_DATA_PATH" --clean_path "$CLEAN_DATA" 
         done
     done
     wait
